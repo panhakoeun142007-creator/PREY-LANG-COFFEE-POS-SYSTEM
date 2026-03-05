9eaf6335270b01cc0f436a945b1923a3301081e7
Panhakoeun <panha.koeun@student.passerellesnumeriques.org>
2026-03-06 00:26:50 +0700

    Rewiew code and function customer

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/orders', [OrderController::class, 'index']);
+Route::put('/orders/{id}/status', [OrderController::class, 'updateStatus']);
